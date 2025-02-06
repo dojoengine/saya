e877109125fc82f23904b30e3ab157902e40361b
Jonathan LEI <me@xjonathan.dev>
2025-02-06 21:48:46 +0800

    feat: bump Starknet OS to v0.13.3 (#14)

--- scripts/generate_snos.sh
+++ scripts/generate_snos.sh
@@ -11,8 +11,8 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_ROOT=$( dirname -- $SCRIPT_DIR )
 
-CAIRO_VERSION="0.13.2.1"
-COMPILER_VERSION="0.13.2"
+CAIRO_VERSION="0.13.3"
+COMPILER_VERSION="0.13.3"
 
 mkdir -p $REPO_ROOT/programs
 
