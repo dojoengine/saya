f566d24e622101c1bacecad0a6ab677ab51db014
glihm <dev@glihm.net>
2025-02-24 12:03:53 -0700

    fix: enable prove_block info logs

--- bin/saya/src/main.rs
+++ bin/saya/src/main.rs
@@ -43,7 +43,7 @@ async fn main() -> Result<()> {
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var(
             "RUST_LOG",
-            "info,saya=trace,saya_core=trace,rpc_client=info,prove_block=off",
+            "info,saya=trace,saya_core=trace,rpc_client=info,prove_block=info",
         );
     }
     env_logger::init();
